94314a44068c8039c21476a6504b9e2b52ce2bca
Lathesh <latheshkn01@gmail.com>
2021-03-08 08:05:16 +0530

    Kotlin mvvm

---
+++ app/src/main/java/com/example/mvvmpracticekotlin/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmpracticekotlin.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmpracticekotlin.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
